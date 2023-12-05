8d004e7309cf17a09cc28a0ec96026171f0053ec
Lucy Kwon <ks02ks222@gmail.com>
2023-12-04 21:11:46 -0800

    created files to work on

---
+++ js/homepage.js
@@ -0,0 +1,11 @@
+var search = $("#search").val;
+var format = $("#format")/val;
+
+function submitHandler(event)    {
+    event.preventDefault();
+    
+
+}
+
+
+$("#user-form").on("click", submitHandler)
