f1064158d9aba2868a1dc91fd403fefc9738458d
Lucy Kwon <ks02ks222@gmail.com>
2023-12-05 17:31:17 -0800

    added a submit handler and getmoreinfo function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,47 +1,81 @@
-$(document).ready(function()  {
-    var search = $("#search");
-    var result = $("#result");
-   
-    function submitHandler(event)   {
-        event.preventDefault();
+$(document).ready(function () {
+  var search = $("#search");
+  var result = $("#result");
 
-        var searchTerm = search.val();
-        var selectedFormat = $("#inputGroupSelect03 option:selected").attr("value");
-        
-        if (searchTerm && selectedFormat)   {
-            getSearchTerm(searchTerm, selectedFormat);
-            
-        } 
-    };
+  function submitHandler(event) {
+    event.preventDefault();
 
-    function getSearchTerm(term, format)    {
-        var apiUrl = "https://www.loc.gov/" +format +"/?q=" + term + "&fo=json";
-        fetch(apiUrl)
-            .then(function (response)   {
-                if (response.ok)    {
-                    response.json().then(function (data)    {
-                        console.log(data.results);
-                        if (data.results)   {
-                            result.html="";
-                            for (var i = 0; i < data.results.length; i++)   {
-                                console.log(data.results[i].date);
-                                console.log(data.results[i].title);
-                                var titleEl = $("<li>").text(data.results[i].title);
-                                var dateEl = $("<li>").text(data.results[i].date);
-                                result.append(titleEl);
-                                result.append(dateEl);
+    var searchTerm = search.val();
+    var selectedFormat = $("#inputGroupSelect03 option:selected").attr("value");
 
-                            }
-                        }   else{
-                            result.text("No result found");
-                        }
-                    });
-                }
-            })
+    if (searchTerm && selectedFormat) {
+      getSearchTerm(searchTerm, selectedFormat);
     }
+  }
 
+  function getSearchTerm(term, format) {
+    var apiUrl = "https://www.loc.gov/" + format + "/?q=" + term + "&fo=json";
+    fetch(apiUrl).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          if (data.results) {
+            result.empty();
+            for (var i = 0; i < data.results.length; i++) {
+              var titleEl = $("<li>").text("title: " + data.results[i].title);
+              var dateEl = $("<p>").text("date: " + data.results[i].date);
+              var readMore = $("<a>")
+                .attr("href", "#")
+                .text("read more")
+                .attr("data-index", i);
+              dateEl.append(readMore);
+              titleEl.append(dateEl);
+              result.append(titleEl);
+            }
+          } else {
+            result.text("No result found");
+          }
+        });
+      }
+    });
+  }
 
+  function getMoreInfo(term, format, index) {
+    var apiUrl = "https://www.loc.gov/" + format + "/?q=" + term + "&fo=json";
+    fetch(apiUrl).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          if (data.results && data.results.length > index) {
+            result.children().eq(index).empty();
 
-    $("#user-form").on("click", submitHandler)
+            var titleEl = $("<li>").text("title: " + data.results[index].title);
+            var dateEl = $("<p>").text("date: " + data.results[index].date);
+            var descriptionEl = $("<p>").text(
+              "description: " + data.results[index].description
+            );
+            var urlEl = $("<p>").text("URL: " + data.results[index].url);
+            titleEl.append(dateEl);
+            titleEl.append(descriptionEl);
+            titleEl.append(urlEl);
+            result.children().eq(index).append(titleEl);
+            
+          } else {
+            result.text("No result found");
+          }
+        });
+      }
+    });
+  }
+  function readmoreHandler(event) {
+    var readmoreEl = event.target;
+    var indexNum = readmoreEl.getAttribute("data-index");
+    var searchTerm = search.val();
+    var selectedFormat = $("#inputGroupSelect03 option:selected").attr("value");
 
-})
+    if (searchTerm && selectedFormat) {
+      getMoreInfo(searchTerm, selectedFormat, indexNum);
+    }
+  }
+
+  $("#user-form").on("click", submitHandler);
+  $(document).on("click", "#result a", readmoreHandler);
+});
